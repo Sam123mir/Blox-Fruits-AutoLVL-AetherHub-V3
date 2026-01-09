b33c7e5286ebba2e5b4bff45d8e249a924603edd
Sam123mir <tu-email@example.com>
2026-01-09 00:19:08 -0500

    Fix: Protect all CombatFramework references with pcall

--- blox-fruits.lua
+++ blox-fruits.lua
@@ -2397,74 +2397,98 @@ function TweenTempleLegit()
 	end)
 
     local Client = game.Players.LocalPlayer
-    local STOP = require(Client.PlayerScripts.CombatFramework.Particle)
-    local STOPRL = require(game:GetService("ReplicatedStorage").CombatFramework.RigLib)
-    spawn(function()
-        while task.wait() do
-            pcall(function()
-                if not shared.orl then shared.orl = STOPRL.wrapAttackAnimationAsync end
-                if not shared.cpc then shared.cpc = STOP.play end
-                    STOPRL.wrapAttackAnimationAsync = function(a,b,c,d,func)
-                    local Hits = STOPRL.getBladeHits(b,c,d)
-                    if Hits then
-                        if _G.FastAttack then
-                            STOP.play = function() end
-                            a:Play(0.01,0.01,0.01)
-                            func(Hits)
-                            STOP.play = shared.cpc
-                            wait(a.length * 0.5)
-                            a:Stop()
-                        else
-                            a:Play()
-                        end
-                    end
-                end
-            end)
+    local STOP, STOPRL = nil, nil
+    
+    pcall(function()
+        -- Wait for CombatFramework to load
+        if Client:FindFirstChild("PlayerScripts") and Client.PlayerScripts:FindFirstChild("CombatFramework") then
+            STOP = require(Client.PlayerScripts.CombatFramework.Particle)
+            STOPRL = require(game:GetService("ReplicatedStorage").CombatFramework.RigLib)
         end
     end)
+    
+    if STOP and STOPRL then
+        spawn(function()
+            while task.wait() do
+                pcall(function()
+                    if not shared.orl then shared.orl = STOPRL.wrapAttackAnimationAsync end
+                    if not shared.cpc then shared.cpc = STOP.play end
+                        STOPRL.wrapAttackAnimationAsync = function(a,b,c,d,func)
+                        local Hits = STOPRL.getBladeHits(b,c,d)
+                        if Hits then
+                            if _G.FastAttack then
+                                STOP.play = function() end
+                                a:Play(0.01,0.01,0.01)
+                                func(Hits)
+                                STOP.play = shared.cpc
+                                wait(a.length * 0.5)
+                                a:Stop()
+                            else
+                                a:Play()
+                            end
+                        end
+                    end
+                end)
+            end
+        end)
+    end
 
 function GetBladeHit()
-    local CombatFrameworkLib = debug.getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))
-    local CmrFwLib = CombatFrameworkLib[2]
-    local p13 = CmrFwLib.activeController
-    local weapon = p13.blades[1]
-    if not weapon then 
-        return weapon
-    end
-    while weapon.Parent ~= game.Players.LocalPlayer.Character do
-        weapon = weapon.Parent 
-    end
-    return weapon
+    local result = nil
+    pcall(function()
+        if game:GetService("Players").LocalPlayer:FindFirstChild("PlayerScripts") and 
+           game:GetService("Players").LocalPlayer.PlayerScripts:FindFirstChild("CombatFramework") then
+            local CombatFrameworkLib = debug.getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))
+            local CmrFwLib = CombatFrameworkLib[2]
+            local p13 = CmrFwLib.activeController
+            local weapon = p13.blades[1]
+            if not weapon then 
+                result = weapon
+                return
+            end
+            while weapon.Parent ~= game.Players.LocalPlayer.Character do
+                weapon = weapon.Parent 
+            end
+            result = weapon
+        end
+    end)
+    return result
 end
 function AttackHit()
-    local CombatFrameworkLib = debug.getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))
-    local CmrFwLib = CombatFrameworkLib[2]
-    local plr = game.Players.LocalPlayer
-    for i = 1, 1 do
-        local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(plr.Character,{plr.Character.HumanoidRootPart},60)
-        local cac = {}
-        local hash = {}
-        for k, v in pairs(bladehit) do
-            if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
-                table.insert(cac, v.Parent.HumanoidRootPart)
-                hash[v.Parent] = true
+    pcall(function()
+        if not game:GetService("Players").LocalPlayer:FindFirstChild("PlayerScripts") or
+           not game:GetService("Players").LocalPlayer.PlayerScripts:FindFirstChild("CombatFramework") then
+            return
+        end
+        local CombatFrameworkLib = debug.getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))
+        local CmrFwLib = CombatFrameworkLib[2]
+        local plr = game.Players.LocalPlayer
+        for i = 1, 1 do
+            local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(plr.Character,{plr.Character.HumanoidRootPart},60)
+            local cac = {}
+            local hash = {}
+            for k, v in pairs(bladehit) do
+                if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
+                    table.insert(cac, v.Parent.HumanoidRootPart)
+                    hash[v.Parent] = true
+                end
+            end
+            bladehit = cac
+            if #bladehit > 0 then
+                pcall(function()
+                    CmrFwLib.activeController.timeToNextAttack = 1
+                    CmrFwLib.activeController.attacking = false
+                    CmrFwLib.activeController.blocking = false
+                    CmrFwLib.activeController.timeToNextBlock = 0
+                    CmrFwLib.activeController.increment = 3
+                    CmrFwLib.activeController.hitboxMagnitude = 120
+                    CmrFwLib.activeController.focusStart = 0
+                    game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetBladeHit()))
+                    game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, i, "")
+                end)
             end
         end
-        bladehit = cac
-        if #bladehit > 0 then
-            pcall(function()
-                CmrFwLib.activeController.timeToNextAttack = 1
-                CmrFwLib.activeController.attacking = false
-                CmrFwLib.activeController.blocking = false
-                CmrFwLib.activeController.timeToNextBlock = 0
-                CmrFwLib.activeController.increment = 3
-                CmrFwLib.activeController.hitboxMagnitude = 120
-                CmrFwLib.activeController.focusStart = 0
-                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetBladeHit()))
-                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, i, "")
-            end)
-        end
-    end
+    end)
 end
 spawn(function()
     while wait(.1) do
